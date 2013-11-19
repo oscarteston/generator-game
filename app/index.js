b40f39c7fa3ca11f9a284e846b83e070757d293a
rafinskipg <rafinskipg@gmail.com>
2013-11-19 22:27:14 +0100

    feat(index.js): added more things

--- app/index.js
+++ app/index.js
@@ -35,25 +35,134 @@ GameGenerator.prototype.askFor = function askFor() {
     }, {
       value: 'crafty',
       name: 'Crafty'
-    }];
+    },
+    {
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Bootstrap for Sass',
+      value: 'compassBootstrap',
+      checked: true
+    }, {
+      name: 'Modernizr',
+      value: 'includeModernizr',
+      checked: true
+    }]
+  }];
 
   this.prompt(prompts, function (props) {
+    var features = answers.features;
+
+    function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.compassBootstrap = hasFeature('compassBootstrap');
+    this.includeModernizr = hasFeature('includeModernizr');
+
     this.library = props.library;
     console.log(this.library);
-    
+
     cb();
   }.bind(this));
 };
 
-GameGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
-};
 
 GameGenerator.prototype.projectfiles = function projectfiles() {
   this.copy('editorconfig', '.editorconfig');
   this.copy('jshintrc', '.jshintrc');
+};
+
+AppGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
+};
+
+AppGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+AppGenerator.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+AppGenerator.prototype.bower = function bower() {
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('_bower.json', 'bower.json');
+};
+
+AppGenerator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+AppGenerator.prototype.editorConfig = function editorConfig() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+AppGenerator.prototype.h5bp = function h5bp() {
+  this.copy('favicon.ico', 'app/favicon.ico');
+  this.copy('404.html', 'app/404.html');
+  this.copy('robots.txt', 'app/robots.txt');
+  this.copy('htaccess', 'app/.htaccess');
+};
+
+AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
+  if (this.compassBootstrap) {
+    this.copy('main.scss', 'app/styles/main.scss');
+  } else {
+    this.copy('main.css', 'app/styles/main.css');
+  }
+};
+
+AppGenerator.prototype.writeIndex = function writeIndex() {
+
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+  this.indexFile = this.engine(this.indexFile, this);
+  this.indexFile = this.appendScripts(this.indexFile, 'scripts/main.js', [
+    'scripts/main.js'
+  ]);
+
+  if (this.coffee) {
+    this.indexFile = this.appendFiles({
+      html: this.indexFile,
+      fileType: 'js',
+      optimizedPath: 'scripts/coffee.js',
+      sourceFileList: ['scripts/hello.js'],
+      searchPath: '.tmp'
+    });
+  }
+
+  if (this.compassBootstrap) {
+    // wire Twitter Bootstrap plugins
+    this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+      'bower_components/sass-bootstrap/js/affix.js',
+      'bower_components/sass-bootstrap/js/alert.js',
+      'bower_components/sass-bootstrap/js/dropdown.js',
+      'bower_components/sass-bootstrap/js/tooltip.js',
+      'bower_components/sass-bootstrap/js/modal.js',
+      'bower_components/sass-bootstrap/js/transition.js',
+      'bower_components/sass-bootstrap/js/button.js',
+      'bower_components/sass-bootstrap/js/popover.js',
+      'bower_components/sass-bootstrap/js/carousel.js',
+      'bower_components/sass-bootstrap/js/scrollspy.js',
+      'bower_components/sass-bootstrap/js/collapse.js',
+      'bower_components/sass-bootstrap/js/tab.js'
+    ]);
+  }
+};
+
+AppGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/scripts');
+  this.mkdir('app/styles');
+  this.mkdir('app/images');
+  this.write('app/index.html', this.indexFile);
+
+  if (this.coffee) {
+    this.write('app/scripts/hello.coffee', this.mainCoffeeFile);
+  }
+
+  this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
 };
